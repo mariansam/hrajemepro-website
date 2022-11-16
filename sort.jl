817edfdb2c66e624b49934741d33b18f8c5e7738
chamik <jakub.hampl@email.cz>
2022-11-16 11:04:28 +0100

    12th

---
+++ sort.jl
@@ -0,0 +1,5 @@
+A = sort([(0, "1.A"), (0, "Kvinta"), (0, "2.B"), (0, "3.A"), (0, "Septima"), (0, "4.B"), (0, "CT")], by=first)
+B = sort([(0, "1.B"), (0, "2.A"), (0, "Sexta"), (0, "3.B"), (0, "4.A"), (0, "Oktáva"), (0, "Výběr NG"), (0, "GJP Praha")], by=first)
+
+println("Skupina A: $A")
+println("Skupina B: $B")
